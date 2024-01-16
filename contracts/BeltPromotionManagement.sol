58088a92709856a757c988ac86229a80489b9a85
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-01-16 14:02:07 +0100

    Work on permissions so that only black belts can promote judokas

--- contracts/BeltPromotionManagement.sol
+++ contracts/BeltPromotionManagement.sol
@@ -6,23 +6,18 @@ import "./JudoBeltStorage.sol";
 contract BeltPromotionManagement {
     JudoBeltStorage private beltStorage;
 
-    // Event to be emitted when a judoka is promoted
     event JudokaPromoted(uint256 indexed judokaId, JudoBeltStorage.BeltLevel newBeltLevel);
 
     constructor(address _beltStorageAddress) {
         beltStorage = JudoBeltStorage(_beltStorageAddress);
     }
 
-    // Function to promote a judoka to a higher belt level
     function promoteJudoka(uint256 _judokaId, JudoBeltStorage.BeltLevel _newBeltLevel) public {
-        require(_judokaId > 0 && _judokaId <= beltStorage.judokaCount(), "Judoka does not exist.");
-        JudoBeltStorage.BeltLevel currentBeltLevel = beltStorage.getJudoka(_judokaId).beltLevel;
-        require(uint(_newBeltLevel) > uint(currentBeltLevel), "New belt level must be higher than current level.");
-        
-        // Update the belt level in the JudoBeltStorage contract
-        beltStorage.setBeltLevel(_judokaId, _newBeltLevel);
+require(beltStorage.getJudoka(_judokaId).id == _judokaId, "Judoka does not exist.");
+    JudoBeltStorage.BeltLevel currentBeltLevel = beltStorage.getJudoka(_judokaId).beltLevel;
+    require(uint(_newBeltLevel) > uint(currentBeltLevel), "New belt level must be higher than current level.");
 
-        // Emit an event for successful promotion
-        emit JudokaPromoted(_judokaId, _newBeltLevel);
-    }
+    beltStorage.setBeltLevel(_judokaId, _newBeltLevel);
+    emit JudokaPromoted(_judokaId, _newBeltLevel);
+}
 }
