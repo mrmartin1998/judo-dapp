c17cc505d9da86523ff2421d395618c230f648e9
Martin Emil Brabenec <martinemilbrabenec@gmail.com>
2024-01-19 10:43:24 +0100

    New contract create for judo competitions

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,20 @@
 const JudoBeltSystem = artifacts.require("JudoBeltSystem");
+const JudoCompetition = artifacts.require("JudoCompetition");
 
 module.exports = async function(deployer) {
     try {
         // Deploy the JudoBeltSystem contract
         await deployer.deploy(JudoBeltSystem);
         const beltSystemInstance = await JudoBeltSystem.deployed();
+
+        // Deploy the JudoCompetition contract
+        await deployer.deploy(JudoCompetition, beltSystemInstance.address);
+        const competitionInstance = await JudoCompetition.deployed();
     
+        // Output the addresses of the deployed contracts
         console.log("Deployed addresses:");
         console.log(`JudoBeltSystem: ${beltSystemInstance.address}`);
+        console.log(`JudoCompetition: ${competitionInstance.address}`);
     } catch (error) {
         console.error("Error deploying contracts:", error);
     }
